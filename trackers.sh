14dafad8965ea334ac2460e3393613f99781f4c5
MR JHON <151718020+MR-JHONz@users.noreply.github.com>
2023-12-15 09:07:48 +0700

    Add files via upload

---
+++ trackers.sh
@@ -0,0 +1,36 @@
+#!/bin/usr/sh
+
+clear #hapus layar
+q="-e"
+e="echo"
+s="sleep 1"
+ss="sleep 2"
+sss="sleep 3"
+m="\033[1;31m"
+h="\033[1;32m"
+k="\033[1;33m"
+b="\033[1;34m"
+bl="\033[1;36m"
+p="\033[1;37m"
+clear
+echo $q $m" _______                             _ _               "
+echo $q $m"(_______)                           | | |              "
+echo $q $m" _        ____ _   _  ____ ____ ____| | | ____  ____   "
+echo $q $m"| |      / ___) | | |/ _  ) ___) _  | | |/ _  )/ ___)  "
+echo $q $p"| |_____| |   | |_| ( (/ ( (__( ( | | | ( (/ /| |"
+echo $q $p" \______)_|    \____|\____)____)_||_|_|_|\____)_|"
+echo $q $m"TRACKERS PHONE NAME"
+echo $q $p"===========================================================                                                       "
+date |lolcat
+echo $q $h"Author : MrJhon"
+echo $q $h"Yt     : CYBERPEMULA"
+echo $q $p"Github : github.com/MR-JHONz"
+echo $q $p"Contact: +628xxxxxx"
+read -p "Enter untuk melanjutkan--->" enter
+echo $q $h"SAYA BEKERJA SAMA DENGAN WEBSITE TRUECALLER"
+sleep 2
+echo $q $m"GUNAKAN SCRIPT SAYA DENGAN BIJAK!!!"
+sleep 4
+echo $q $h"WEBSITE INI BERTUJUAN UNTUK MENGETAHUI NAMA PEMILIK NOMOR"
+sleep 1
+xdg-open "https://www.truecaller.com/id-id"
